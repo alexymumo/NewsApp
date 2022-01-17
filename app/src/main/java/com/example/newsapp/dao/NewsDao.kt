e31aa0ece9cfa74d425e0fc89c6a29ac18f6be95
Alex-mumo <alexymumo2000@gmail.com>
2022-01-18 00:11:09 +0300

    update

--- app/src/main/java/com/example/newsapp/dao/NewsDao.kt
+++ app/src/main/java/com/example/newsapp/dao/NewsDao.kt
@@ -1,11 +1,17 @@
 package com.example.newsapp.dao
 
-import androidx.room.Dao
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.newsapp.models.Articles
 
 @Dao
 interface NewsDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun saveArticle()
 
-    suspend fun getNews() {
+    @Query("SELECT * FROM article_table")
+    fun fetchArticles(): LiveData<List<Articles>>
 
-    }
+    @Delete
+    suspend fun deleteArticles(articles: Articles)
 }
---
+++ app/src/main/java/com/example/newsapp/db/NewsDatabase.kt
@@ -0,0 +1,6 @@
+package com.example.newsapp.db
+
+import androidx.room.RoomDatabase
+
+abstract class NewsDatabase : RoomDatabase() {
+}
