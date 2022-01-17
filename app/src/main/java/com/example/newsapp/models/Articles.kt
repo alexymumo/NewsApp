69498d98fd232dfcae062135114e065270abc31c
Alex-mumo <alexymumo2000@gmail.com>
2022-01-17 23:30:32 +0300

    updated models

--- app/src/main/java/com/example/newsapp/models/Articles.kt
+++ app/src/main/java/com/example/newsapp/models/Articles.kt
@@ -7,12 +7,12 @@ import java.io.Serializable
 @Entity(tableName = "article_table")
 data class Articles(
     @PrimaryKey(autoGenerate = true)
-    var id : Int,
-    val author: String,
-    val content: String,
-    val description: String,
-    val publishedAt: Sources,
-    val title: String,
-    val url: String,
-    val urlImage: String
+    var id : Int?,
+    val author: String?,
+    val content: String?,
+    val description: String?,
+    val publishedAt: Sources?,
+    val title: String?,
+    val url: String?,
+    val urlImage: String?
 ) : Serializable
