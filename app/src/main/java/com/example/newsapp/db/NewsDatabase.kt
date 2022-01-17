79dd57177ce95606eaeec8d35eb9a196253ad76c
Alex-mumo <alexymumo2000@gmail.com>
2022-01-18 00:29:26 +0300

    added converters

--- app/src/main/java/com/example/newsapp/db/NewsDatabase.kt
+++ app/src/main/java/com/example/newsapp/db/NewsDatabase.kt
@@ -1,6 +1,11 @@
 package com.example.newsapp.db
 
+import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.newsapp.dao.NewsDao
 
+@Database(entities = [NewsDao::class], version = 1, exportSchema = false)
 abstract class NewsDatabase : RoomDatabase() {
+
+    //private val newsDao: NewsDao()
 }
---
+++ app/src/main/java/com/example/newsapp/db/mappers/Converters.kt
@@ -0,0 +1,16 @@
+package com.example.newsapp.db.mappers
+
+import androidx.room.TypeConverters
+import com.example.newsapp.models.Sources
+import javax.xml.transform.Source
+
+class Converters {
+    @TypeConverters
+    fun fromSource(sources: Sources): String {
+        return sources.name
+    }
+    @TypeConverters
+    fun toSource(name: String): Sources {
+        return Sources(name, name)
+    }
+}
