c8b509fc449c272468eceff7d2d9431bf9dd776f
Alex-mumo <alexymumo2000@gmail.com>
2022-01-18 06:28:40 +0300

    added application

--- app/src/main/java/com/example/newsapp/NewsApplication.kt
+++ app/src/main/java/com/example/newsapp/NewsApplication.kt
@@ -1,6 +1,28 @@
 package com.example.newsapp
 
 import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.error.KoinAppAlreadyStartedException
+import org.koin.core.logger.Level
 
 class NewsApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        initKoin()
+    }
+
+    private fun initKoin() {
+        try {
+            startKoin {
+                androidLogger(Level.ERROR)
+                androidContext(applicationContext)
+
+            }
+        }catch (error : KoinAppAlreadyStartedException){
+            //
+
+        }
+    }
 }
