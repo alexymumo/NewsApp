b8dae90b5bc11ea985c9c4a43a7e39a0f24e570c
Alex-mumo <alexymumo2000@gmail.com>
2022-01-18 10:51:28 +0300

    added newsviewModelfactory

--- app/src/main/java/com/example/newsapp/viewmodels/NewsViewModel.kt
+++ app/src/main/java/com/example/newsapp/viewmodels/NewsViewModel.kt
@@ -1,6 +1,24 @@
 package com.example.newsapp.viewmodels
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.example.newsapp.models.Articles
+import com.example.newsapp.repository.NewsRepository
+import kotlinx.coroutines.launch
+import java.lang.IllegalArgumentException
 
-class NewsViewModel : ViewModel() {
+class NewsViewModel(application: Application, private val newsRepository: NewsRepository) : AndroidViewModel(application) {
+
+    fun saveArticle(articles: Articles) = viewModelScope.launch {
+        newsRepository.saveArticle(articles)
+    }
+
+    fun deleteArticle(articles: Articles) = viewModelScope.launch {
+        newsRepository.deleteArticle(articles)
+    }
+
+    fun fetchAllArticles(articles: Articles) = newsRepository.getAllArticles()
 }
---
+++ app/src/main/java/com/example/newsapp/viewmodels/NewsViewModelFactory.kt
@@ -0,0 +1,17 @@
+package com.example.newsapp.viewmodels
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.newsapp.repository.NewsRepository
+import java.lang.IllegalArgumentException
+
+class NewsViewModelFactory(val application: Application,val newsRepository: NewsRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(NewsViewModel::class.java)){
+            @Suppress("UNCHECKED CAST")
+            return NewsViewModel(application,newsRepository)  as T
+        }
+        throw IllegalArgumentException("Unknown viewmodel class")
+    }
+}
