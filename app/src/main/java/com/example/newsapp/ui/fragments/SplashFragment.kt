5b774bca7cf11c06d134fb51790dc15e347a2bac
Alex-mumo <alexymumo2000@gmail.com>
2022-01-25 00:54:34 +0300

    update

--- app/src/main/java/com/example/newsapp/ui/fragments/SplashFragment.kt
+++ app/src/main/java/com/example/newsapp/ui/fragments/SplashFragment.kt
@@ -6,17 +6,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.newsapp.R
-import com.example.newsapp.ui.adapters.NewsAdapter
-import com.example.newsapp.ui.viewmodels.NewsViewModel
 
-class TechFragment : Fragment() {
-    private lateinit var newsViewModel: NewsViewModel
-    private lateinit var newsAdapter: NewsAdapter
+class SplashFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_tech, container, false)
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+
+
     }
 }
