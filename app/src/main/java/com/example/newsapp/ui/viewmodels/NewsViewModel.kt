e9694c93b715da5c6ca9d44e8723975ea9fc5db0
Alex-mumo <alexymumo2000@gmail.com>
2022-01-25 11:48:37 +0300

    update

--- app/src/main/java/com/example/newsapp/ui/viewmodels/NewsViewModel.kt
+++ app/src/main/java/com/example/newsapp/ui/viewmodels/NewsViewModel.kt
@@ -2,12 +2,39 @@ package com.example.newsapp.ui.viewmodels
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import com.bumptech.glide.load.engine.Resource
 import com.example.newsapp.data.local.entities.Articles
 import com.example.newsapp.data.repository.NewsRepository
 import kotlinx.coroutines.launch
 
 class NewsViewModel(application: Application, private val newsRepository: NewsRepository) : AndroidViewModel(application) {
+
+    var breakingNewsPage = 1
+
+    init {
+        getBreakingNews()
+        getTechNews()
+        getTrendingNews()
+        getSportsNews()
+    }
+
+    private fun getSportsNews() {
+        TODO("Not yet implemented")
+    }
+
+    private fun getTrendingNews() {
+        TODO("Not yet implemented")
+    }
+
+    private fun getTechNews() {
+        TODO("Not yet implemented")
+    }
+
+    private fun getBreakingNews() {
+        TODO("Not yet implemented")
+    }
 
     fun saveArticle(articles: Articles) = viewModelScope.launch {
         newsRepository.saveArticle(articles)
