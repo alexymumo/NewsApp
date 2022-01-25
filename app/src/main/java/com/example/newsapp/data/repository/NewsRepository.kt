cb31501add1664c14b1464ce54c10e217ad0cf2e
Alex-mumo <alexymumo2000@gmail.com>
2022-01-25 10:30:04 +0300

    update

--- app/src/main/java/com/example/newsapp/data/repository/NewsRepository.kt
+++ app/src/main/java/com/example/newsapp/data/repository/NewsRepository.kt
@@ -4,21 +4,7 @@ import com.example.newsapp.data.local.db.NewsDatabase
 import com.example.newsapp.data.local.entities.Articles
 import com.example.newsapp.data.network.NewsApi
 
-class NewsRepository(private val newsDatabase: NewsDatabase, private val newsApi: NewsApi) {
-
-    /*suspend fun getTechNews(countryCode: String, pageNo: Int) =
-        newsApi.getTechNews(countryCode, pageNo)
-
-    suspend fun getBreakingNews(countryCode: String, pageNo: Int) =
-        newsApi.getBreakingNews(countryCode, pageNo)
-
-    suspend fun getSportsNews(countryCode: String, pageNo: Int) =
-        newsApi.getSportsNews(countryCode, pageNo)
-
-    suspend fun getTrendingNews(countryCode: String, pageNo: Int) =
-        newsApi.getTrendingNews(countryCode, pageNo)
-
-     */
+class NewsRepository(private val newsDatabase: NewsDatabase) {
     suspend fun saveArticle(articles: Articles) = newsDatabase.newsDao().saveArticle(articles)
 
     suspend fun deleteArticle(articles: Articles) = newsDatabase.newsDao().deleteArticles(articles)
--- app/src/main/java/com/example/newsapp/ui/fragments/TrendingFragment.kt
+++ app/src/main/java/com/example/newsapp/ui/fragments/TrendingFragment.kt
@@ -5,19 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.example.newsapp.R
 import com.example.newsapp.ui.adapters.NewsAdapter
 import com.example.newsapp.ui.viewmodels.NewsViewModel
 
 
 class TrendingFragment : Fragment() {
-    private lateinit var newsViewModel: NewsViewModel
+
+    private val viewModels: NewsViewModel by viewModels()
     private lateinit var newsAdapter: NewsAdapter
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_trending, container, false)
     }
 }
