dbbcf781cec7ce8c476c33fd55ac5cabcf2d55b3
Alex-mumo <alexymumo2000@gmail.com>
2022-01-26 08:41:18 +0300

    update

--- app/src/main/java/com/example/newsapp/ui/viewmodels/NewsViewModel.kt
+++ app/src/main/java/com/example/newsapp/ui/viewmodels/NewsViewModel.kt
@@ -5,12 +5,26 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.example.newsapp.data.local.entities.Articles
+import com.example.newsapp.data.local.entities.NewsResponse
 import com.example.newsapp.data.repository.NewsRepository
+import com.example.newsapp.utils.Resource
 import kotlinx.coroutines.launch
 
 class NewsViewModel(application: Application, private val newsRepository: NewsRepository) : AndroidViewModel(application) {
 
-    var breakingNewsPage = 1
+    var sportsNewsPage = 1
+    val sportsNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    var sportsNewsResponse: NewsResponse? = null
+
+    var trendingNewsPage = 1
+    val trendingNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    var trendingNewsResponse: NewsResponse? = null
+
+    var techNewsPage = 1
+    val techNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    var techNewsResponse: NewsResponse? = null
+
+
 
     init {
         getBreakingNews()
@@ -19,20 +33,20 @@ class NewsViewModel(application: Application, private val newsRepository: NewsRe
         getSportsNews()
     }
 
-    private fun getSportsNews() {
-        TODO("Not yet implemented")
+    private fun getSportsNews() = viewModelScope.launch {
+
     }
 
-    private fun getTrendingNews() {
-        TODO("Not yet implemented")
+    private fun getTrendingNews() = viewModelScope.launch {
+
     }
 
-    private fun getTechNews() {
-        TODO("Not yet implemented")
+    private fun getTechNews() = viewModelScope.launch {
+
     }
 
-    private fun getBreakingNews() {
-        TODO("Not yet implemented")
+    private fun getBreakingNews() = viewModelScope.launch {
+
     }
 
     fun saveArticle(articles: Articles) = viewModelScope.launch {
