3f74ea91b5db0532ebf1f2ef3f3caa684a55e2c5
Alex-mumo <alexymumo2000@gmail.com>
2022-01-27 09:50:35 +0300

    update

--- app/src/main/java/com/example/newsapp/ui/fragments/ArticleFragment.kt
+++ app/src/main/java/com/example/newsapp/ui/fragments/ArticleFragment.kt
@@ -5,17 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.View
 import com.example.newsapp.R
 import com.example.newsapp.ui.viewmodels.NewsViewModel
-import com.google.android.material.snackbar.Snackbar
 
 class ArticleFragment : Fragment(R.layout.fragment_article) {
-    lateinit var newsViewModel: NewsViewModel
+    private lateinit var newsViewModel: NewsViewModel
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        fab.setOnclickListener(View.OnClickListener {
-            newsViewModel.saveArticle(articles)
-            Snackbar.make(view, "saved", Snackbar.LENGTH_SHORT).show()
-        })
     }
 }
