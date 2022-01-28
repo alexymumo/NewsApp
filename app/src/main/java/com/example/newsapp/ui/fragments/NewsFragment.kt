71f30ba808f41e837096f9610006a331424b1bb7
Alex-mumo <alexymumo2000@gmail.com>
2022-01-29 01:26:32 +0300

    update

--- app/src/main/java/com/example/newsapp/ui/fragments/NewsFragment.kt
+++ app/src/main/java/com/example/newsapp/ui/fragments/NewsFragment.kt
@@ -5,12 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
 import com.example.newsapp.databinding.FragmentNewsBinding
 import com.example.newsapp.ui.viewmodels.NewsViewModel
+import java.util.*
 
 class NewsFragment : Fragment() {
     private lateinit var binding: FragmentNewsBinding
-    private lateinit var newsViewModel: NewsViewModel
+    lateinit var newsViewModel: NewsViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
