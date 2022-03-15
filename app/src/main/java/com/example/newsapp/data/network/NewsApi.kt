b2e74b90f70bfd0e0e374b1a15d89a808432111e
Alex-mumo <alexymumo2000@gmail.com>
2022-03-15 20:51:16 +0300

    update

--- app/src/main/java/com/example/newsapp/data/network/NewsApi.kt
+++ app/src/main/java/com/example/newsapp/data/network/NewsApi.kt
@@ -2,9 +2,30 @@ package com.example.newsapp.data.network
 
 import com.example.newsapp.data.local.entities.NewsResponse
 import com.example.newsapp.utils.Constants.Companion.API_KEY
+import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface NewsApi {
-    @GET("?key=$API_KEY")
-    suspend fun getAllNews(): NewsResponse
+    @GET("v2/top-headlines")
+    suspend fun searchNews(
+        @Query("q") searchQuery: String,
+        @Query("page") pageNumber: Int = 1,
+        @Query("apiKey") apiKey: String = API_KEY
+    ): Response<NewsResponse>
+
+    @GET("v2/top-headlines")
+    suspend fun getBreakingNews(
+        @Query("country") countryCode: String = "us",
+        @Query("page") pageNumber: Int = 1,
+        @Query("apiKey") apiKey: String = API_KEY
+    ): Response<NewsResponse>
+
+    @GET("v2/top-headlines")
+    suspend fun getTechNews(
+        @Query("country") countryCode: String = "us",
+        @Query("page") pageNumber: Int = 1,
+        @Query("apiKey") apiKey: String = API_KEY,
+        @Query("category") categoryString: String = "technology"
+    ): Response<NewsResponse>
 }
--- app/src/main/java/com/example/newsapp/ui/viewmodels/NewsViewModel.kt
+++ app/src/main/java/com/example/newsapp/ui/viewmodels/NewsViewModel.kt
@@ -3,29 +3,20 @@ package com.example.newsapp.ui.viewmodels
 
 import androidx.lifecycle.*
 import com.example.newsapp.data.local.entities.News
-import com.example.newsapp.data.local.entities.NewsResponse
 import com.example.newsapp.data.repository.NewsRepository
-import com.example.newsapp.utils.Resource
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
 class NewsViewModel @Inject constructor(
-    private val newsRepository: NewsRepository,
-    private
-
+    private val newsRepository: NewsRepository
 ): ViewModel() {
-
-    //private val _newResponse: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
-    //val newsResponse: LiveData<Resource<NewsResponse>>
-    //get() = _newResponse
 
     private val _news = MutableLiveData<MutableList<News>>()
     val news: LiveData<MutableList<News>> = _news
 
     fun fetchNews() = viewModelScope.launch {
 
-        _newResponse.value = newsRepository.getAllNews()
     }
 }
 
